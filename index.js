f5c1d55409e795b826db4265dd4036bd44caac6c
Louis <s10257445@connect.np.edu.sg>
2023-11-29 23:44:17 +0800

    updated to index.html and index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+    // JavaScript for the image slider
+    let currentIndex = 0;
+    const slides = document.querySelector('.slides');
+    const totalSlides = document.querySelectorAll('.slide').length;
+
+    function showSlide(index) {
+        if (index < 0) {
+            currentIndex = totalSlides - 1;
+        } else if (index >= totalSlides) {
+            currentIndex = 0;
+        } else {
+            currentIndex = index;
+        }
+
+        const transformValue = -100 * currentIndex + '%';
+        slides.style.transform = 'translateX(' + transformValue + ')';
+    }
+
+    function nextSlide() {
+        showSlide(currentIndex + 1);
+    }
+
+    function prevSlide() {
+        showSlide(currentIndex - 1);
+    }
+
+    setInterval(nextSlide, 3000); // Change slide every 3 seconds
